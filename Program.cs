2424bc9768fc837f1f83bf0b6240c0b3e5d66433
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2024-11-06 20:10:01 -0500

    Program.cs + assignment outline
    Created Program.cs
    
    Outlined assignment instructions using single line comments

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Building_Blocks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //make some vars, print them out
+            //make a method that prints out instructor name
+            //make a method that takes in 2 nums, adds them, then returns
+            //print out result
+        }
+    }
+}
